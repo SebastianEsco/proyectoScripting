eda14264949bed646bb5d2fc657c975965ff391a
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-02-14 11:32:45 -0500

    Avances botones

---
+++ Assets/proyecto/Scripts/BotonInstrucciones.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonInstrucciones : MonoBehaviour
+{
+    public void OnClickInstrucciones()
+    {
+        Debug.Log("ClickInstrucciones");
+    }
+}
---
+++ Assets/proyecto/Scripts/BotonSalir.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonSalir : MonoBehaviour
+{
+    public void OnClickSalir()
+    {
+        Debug.Log("ClickSalir");
+    }
+}

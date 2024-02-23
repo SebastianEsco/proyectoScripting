987603b49a0d1ff834e82e412e9fbf303b881c89
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-02-23 00:19:36 -0500

    Nivel 1 completado

--- Assets/proyecto/Scripts/BotonSalir.cs
+++ Assets/proyecto/Scripts/BotonSalir.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,6 @@ public class BotonSalir : MonoBehaviour
 {
     public void OnClickSalir()
     {
-        Debug.Log("ClickSalir");
+        Application.Quit();
     }
 }

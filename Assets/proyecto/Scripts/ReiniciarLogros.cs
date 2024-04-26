aa1d1c66d127cc66017ca2bb1cee8b07f976441a
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-04-26 12:53:45 -0500

    Commit final para la exposición

--- Assets/proyecto/Scripts/ReiniciarLogros.cs
+++ Assets/proyecto/Scripts/ReiniciarLogros.cs
@@ -10,6 +10,18 @@ public class ReiniciarLogros : MonoBehaviour
         foreach(var logro in MMAchievementManager.AchievementsList)
         {
             MMAchievementManager.LockAchievement(logro.AchievementID);
+            if(logro.AchievementID == "Recolectador")
+            {
+                logro.SetProgress(0);
+            }
+            if (logro.AchievementID == "Recolectador2")
+            {
+                logro.SetProgress(0);
+            }
+            if (logro.AchievementID == "Recolectador3")
+            {
+                logro.SetProgress(0);
+            }
         }
     }
 }

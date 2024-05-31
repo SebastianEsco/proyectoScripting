e0bf883d3f65c83e6ebdca0d41b4ad04ff9d6d1d
andruAJ <andres.giraldov@upb.edu.co>
2024-05-31 17:04:34 -0500

    solucionado problema temporizador nivel 3

--- Assets/proyecto/Scripts/SelectorDeDificultad.cs
+++ Assets/proyecto/Scripts/SelectorDeDificultad.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SelectorDeDificultad : MonoBehaviour
 {
     public int dificultad;
+
     public void EscogerDificultad(int dificultadElegida)
     {
         dificultad = dificultadElegida;
@@ -12,23 +13,52 @@ public class SelectorDeDificultad : MonoBehaviour
 
     public void SettearDificultad()
     {
-        if (GameObject.Find("EnemigosNormal"))
-        {
-            if (dificultad == 1) //DIFICULTAD FACIL
-            {
-                GameObject.Find("EnemigosNormal").SetActive(false);
-                GameObject.Find("PlataformasExtraNormal").SetActive(false);
-                GameObject.Find("PlataformasExtraFacil").SetActive(true);
-            }
-            if (dificultad == 2) //DIFICULTAD NORMAL
-            {
-                GameObject.Find("PlataformasExtraFacil").SetActive(false);
-            }
-            if (dificultad == 3) //DIFICULTAD DIFICIL
-            {
-                GameObject.Find("UICamera/Canvas/Temporizador").SetActive(true);
+        // Encuentra el GameObject del temporizador
+        GameObject temporizador = GameObject.Find("UICamera/Canvas/Temporizador");
 
-            }
+        // Verifica si el GameObject del temporizador fue encontrado
+        if (temporizador != null)
+        {
+            // Obtiene el componente Temporizador del GameObject
+            Temporizador script = temporizador.GetComponent<Temporizador>();
+
+            
+                if (dificultad == 1) // DIFICULTAD FÁCIL
+                {
+                    GameObject.Find("EnemigosNormal").SetActive(false);
+                    GameObject.Find("PlataformasExtraNormal").SetActive(false);
+                    GameObject.Find("PlataformasExtraFacil").SetActive(true);
+                    temporizador.SetActive(false);
+
+                    if (script != null)
+                    {
+                        script.enabled = false;
+                    }
+                }
+                else if (dificultad == 2) // DIFICULTAD NORMAL
+                {
+                    GameObject.Find("PlataformasExtraFacil").SetActive(false);
+                    temporizador.SetActive(false);
+
+                    if (script != null)
+                    {
+                        script.enabled = false;
+                    }
+                }
+                else if (dificultad == 3) // DIFICULTAD DIFÍCIL
+                {
+                    temporizador.SetActive(true);
+
+                    if (script != null)
+                    {
+                        script.enabled = true;
+                    }
+                }         
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró el GameObject 'Temporizador'.");
         }
     }
 }
+
